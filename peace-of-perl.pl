24d18faab25346b75c0cf871a060b28daceff30a
Christopher Lacy-Hulbert <clacy@yahoo-inc.com>
2013-08-10 11:56:36 +0100

    Added the perl file and updated the readme

---
+++ peace-of-perl.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+listen to, me;
+stop and listen just, once;
+connect with, me;
+join our $worlds;
+
+unless ( my $ideas )
+        { alarm you }
+accept with, open-minds;
+
+push @these, words, upon, our $leaders;
+seek to, change, their-ways;
+push @them, bribe and pray;
+our ($world, $will, $rest, $one, $day);
+
+tell me if( our $only, $hopes );
+split and drift away;
+read these, $lines, one-more, time;
+stop and think, shutdown( this, crime );
+
+open minds, `touch friends`;
+close the-blinds and wait;
+if ( my $words ){ are heard by them }
+$we { may_not_be_too_late }
